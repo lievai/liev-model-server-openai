c2748a1cb25a0fac692699c08eb97372b913db90
LievTecnico <liev.tecnico@inmetrics.com.br>
2024-11-26 16:40:18 +0000

    Refreshing_repo_for_version_1.1.2-oss echo '--- Creating tag ---' git tag 1.1.2-oss

--- model.py
+++ model.py
@@ -1,10 +1,9 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_httpauth import HTTPBasicAuth
 from flask_restful import Api
 import json
-from config.config import Config
-import openai
+from openai import OpenAI
 import logging
 from dotenv import load_dotenv
 
@@ -16,51 +15,34 @@ logger = logging.getLogger(__name__)
 
 logger.info('STARTING model.py')
 
+LIEV_PASSWORD = os.getenv("LIEV_PASSWORD")
+LIEV_USERNAME = os.getenv("LIEV_USERNAME")
+API_KEY = os.getenv("OPENAI_KEY")
+MODEL = os.getenv("MODEL")
 
-config = Config('openai')
-
-LIEV_PASSWORD = config.get("LIEV_PASSWORD")
-LIEV_USERNAME = config.get("LIEV_USERNAME")
-
-MODEL = config.get("MODEL")
+openai = OpenAI(api_key=API_KEY)
 
 app = Flask(__name__)
 api = Api(app)
 auth = HTTPBasicAuth()
 
-@auth.verify_password
-def verify(username, password):
-    """ Verify: Check username and password to allow access to the API"""
-    if not (username and password):
-        return False
-    return username == LIEV_USERNAME and password == LIEV_PASSWORD
-
-
-@app.route('/response')
-@auth.login_required
-def response():
-
-    openai.api_key = config.get("OPENAI_KEY")
-    
+def check_data(request):
     data = request.data
     try:
         data = json.loads(data)
+        if isinstance(data, dict) == False:
+            return False, json.dumps("JSON load conversion problem. Not a dict ! Are you using data payload  ?"), 400
+        return True, data, 200
     except Exception as e:
         logger.error(f"JSON load problem!: {e}", exc_info=True)
-        return json.dumps("JSON load problem !"), 500
-
-    if isinstance(data, dict) == False:
-        return json.dumps("JSON load conversion problem. Not a dict ! Are you using data payload  ?"), 400
+        return False, json.dumps("JSON load problem !"), 500
     
-    temperature = float(data.get('temperature', 1))
-    top_p = float(data.get('top_p', 1))
-    stop_words = data.get('stop_words', [])
-
+def create_messages(data):
     # verifica se existe a chave messages no dict:
     if data.get('messages'):
         # se existir, verifica se existe instruction ou system_msg juntos
         if data.get('instruction') or data.get('system_msg'):
-            return json.dumps("If the messages parameter is passed, the instruction or system_msg parameters must not be passed."), 501
+            return False, json.dumps("If the messages parameter is passed, the instruction or system_msg parameters must not be passed."), 501
         messages = data['messages']
     else:
         instruction = data.get('instruction', "hi")
@@ -75,20 +57,81 @@ def response():
             messages.append({"role": "system", "content": system_msg})
         messages.append({"role": "user", "content": instruction})
 
+    return True, messages, 200
+
+@auth.verify_password
+def verify(username, password):
+    """ Verify: Check username and password to allow access to the API"""
+    if not (username and password):
+        return False
+    return username == LIEV_USERNAME and password == LIEV_PASSWORD
+
+@app.route('/response')
+@auth.login_required
+def response():
+
+    validate, data, code = check_data(request=request)
+    if not validate:
+        return data, code
+
+    temperature = float(data.get('temperature', 1))
+    top_p = float(data.get('top_p', 1))
+    stop_words = data.get('stop_words', [])
+
+    validate, messages, code = create_messages(data=data)
+    if not validate:
+        return messages, code
+
     try:
-        response = openai.ChatCompletion.create(
+        response = openai.chat.completions.create(
             model=MODEL,
             temperature=temperature,
             top_p=top_p,
             messages=messages,
-            stop=stop_words
+            stop=stop_words,
+            stream=False
         )
-        answer = response.choices[0]['message']['content']
+        answer = response.choices[0].message.content
         return json.dumps(answer), 200
     except Exception as e:
         answer = "Error ! : " + str(e)
         logger.error(answer, exc_info=True)
         return json.dumps(answer), 500
+
+@app.route('/stream', methods=['POST'])
+@auth.login_required
+def stream():
+    validate, data, code = check_data(request=request)
+    if not validate:
+        return data, code
+
+    temperature = float(data.get('temperature', 1))
+    top_p = float(data.get('top_p', 1))
+    stop_words = data.get('stop_words', [])
+
+    validate, messages, code = create_messages(data=data)
+    if not validate:
+        return messages, code
+    
+    try:
+        response = openai.chat.completions.create(
+            model=MODEL,
+            temperature=temperature,
+            top_p=top_p,
+            messages=messages,
+            stop=stop_words,
+            stream=True
+        )
+        def generate():
+            for chunk in response:
+                yield chunk.choices[0].delta.content
+        
+        return Response(generate(), mimetype='application/json')
+    
+    except Exception as e:
+        answer = "Error ! : " + str(e)
+        logger.error(answer, exc_info=True)
+        return json.dumps(answer), 500    
 
 
 # DO NOT REMOVE
